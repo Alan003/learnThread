10c7abc9b3665d257818ccc648e5c670ffd757a7
yanzeyu <13144454225>
2019-12-11 11:43:59 +0800

    添加注释

--- src/main/java/com/thread/threadlife/ThreadLifeTest.java
+++ src/main/java/com/thread/threadlife/ThreadLifeTest.java
@@ -14,13 +14,18 @@ public class ThreadLifeTest {
                 try {
                     System.out.println("thread1 waiting");
                     o.wait(1000*5);
-                    //Thread.sleep(1000*2);
+                    //Thread.sleep(1000*20);
                     System.out.println("thread1 after waiting");
                 }catch (InterruptedException e){
                     e.printStackTrace();
                 }
             }
         },"Thread1").start();
+
+        /** Thread.sleep(1000*6);不会释放锁，
+         * Thread2线程继续持有锁，等待6秒后，先输出“thread2 after waiting”，Thread2线程运行完后释放锁，
+         * Thread1线程获得锁后继续运行，输出“thread1 after waiting”
+         */
 
         new Thread(() ->{
             synchronized (o) {
