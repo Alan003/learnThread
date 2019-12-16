2c5b6e039960a184b5ee9c885887c157909280fc
yanzeyu <13144454225>
2019-12-16 14:05:37 +0800

    java api方式配置mybatis

--- src/main/java/com/alan/mybatis/AppConfig.java
+++ src/main/java/com/alan/mybatis/AppConfig.java
@@ -1,6 +1,7 @@
 package com.alan.mybatis;
 
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -11,13 +12,17 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-public class config {
+@MapperScan("com.alan.mapper")
+public class AppConfig {
 
     @Bean
     public DataSource dataSource() throws Exception {
@@ -30,12 +35,11 @@ public class config {
     }
 
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        TransactionFactory transactionFactory =
-                new JdbcTransactionFactory();
-        Environment environment =
-                new Environment("development", transactionFactory, dataSource());
-        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration(environment);
-        return new SqlSessionFactoryBuilder().build(configuration);
+    public SqlSessionFactory sqlSessionFactory () throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*Mapper.xml"));
+        return sqlSessionFactoryBean.getObject();
     }
 }
--- src/main/java/com/alan/mybatis/mybatis_spring.java
+++ src/main/java/com/alan/mybatis/mybatis_spring.java
@@ -1,16 +1,23 @@
 package com.alan.mybatis;
 
 import com.alan.mapper.QueryMapper;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.sql.DataSource;
 import java.util.Map;
+
 
 public class mybatis_spring {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(config.class);
-        QueryMapper bean = applicationContext.getBean(QueryMapper.class);
-        Map list = bean.list();
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        SqlSessionFactory sqlSessionFactory = (SqlSessionFactory)applicationContext.getBean("sqlSessionFactory");
+        //DataSource dataSource = (DataSource) applicationContext.getBean("dataSource");
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        QueryMapper mapper = sqlSession.getMapper(QueryMapper.class);
+        Map list = mapper.list();
         System.out.println(list);
     }
 }
