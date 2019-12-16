f11fd8a7df0dc04d16c88db27b273cc916f85ef0
yanzeyu <13144454225>
2019-12-16 11:04:17 +0800

    xml方式配置mybatis

--- src/main/java/com/alan/mybatis/StartupMybatis.java
+++ src/main/java/com/alan/mybatis/StartupMybatis.java
@@ -2,6 +2,7 @@ package com.alan.mybatis;
 
 import com.alan.mapper.QueryMapper;
 import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
@@ -9,28 +10,18 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.TransactionFactory;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import sun.misc.Resource;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StartupMybatis  {
     public static void main(String[] args) throws Exception {
-
-        Map<String, String> map = new HashMap<>();
-        map.put("driver","com.mysql.jdbc.Driver");
-        map.put("url","jdbc:mysql://47.106.159.61:3306/mall?useUnicode=true&characterEncoding=utf-8");
-        map.put("username","root");
-        map.put("password","root");
-        DataSource dataSource = DruidDataSourceFactory.createDataSource(map);
-
-        TransactionFactory transactionFactory =
-                new JdbcTransactionFactory();
-        Environment environment =
-                new Environment("development", transactionFactory, dataSource);
-        Configuration configuration = new Configuration(environment);
-        configuration.addMappers("com.alan.mapper");
-        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
         QueryMapper queryMapper = session.getMapper(QueryMapper.class);
         Map list = queryMapper.list();
