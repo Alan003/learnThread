d4e957edc1f8902fefda478f08f9798626226cce
yanzeyu <13144454225>
2019-12-16 14:15:01 +0800

    直接从IOC容器取出mapper

--- src/main/java/com/alan/mapper/QueryMapper.java
+++ src/main/java/com/alan/mapper/QueryMapper.java
@@ -6,6 +6,6 @@ import org.springframework.context.annotation.Bean;
 import java.util.Map;
 
 public interface QueryMapper {
-   @Select("select * from pms_brand where id=1")
+   //@Select("select * from pms_brand where id=1")
     Map list();
 }
